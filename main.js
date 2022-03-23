8b2a70ad6f5b3b21f168297060a039a1dc45f163
Amy Westell <98362073+amysfrogs@users.noreply.github.com>
2022-03-22 22:28:26 -0400

    Dual axis chart done, bar chart started
    Need to finish bar chart, stuck on how to add bars from different columns

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ var map = svg.append("g")
 
 Promise.all([
         d3.json("./data/50m.json"),
-        d3.csv("./data/ConsolidatedHappiness.csv")
+        d3.csv("./data/ConsolidatedHappinessWithHomicideData.csv")
     ]).then(d => {
         const world = d[0];
         const data = d[1];
@@ -49,11 +49,13 @@ function drawMap(world, data) {
     var minRank = 0;
     var maxRank = 0;
 
+    console.log(features)
+
     data.forEach(function (d) {
         if(d.Year === selectedYear) {
 
             if(d['Happiness rank'] <= selectedTopN) {
-                details[d['Country or region']] = {
+                details[d['country']] = {
                     rank: +d['Happiness rank'],
                     score: +d['Score']
                 }
@@ -81,7 +83,7 @@ function drawMap(world, data) {
         d.details = details[d.properties.name] ? details[d.properties.name] : {};
     });
 
-
+    console.log(details)
 
     map.append("g")
         .attr('class', 'basemap')
@@ -108,7 +110,7 @@ function drawMap(world, data) {
                 .style("cursor", "pointer");
 
             d3.select(".country")
-                .text(d.properties.name);
+                .text(d.name);
 
             d3.select(".rank")
                 .text(d.details && d.details.rank && "Rank " + d.details.rank || "Rank: -");
@@ -143,40 +145,40 @@ function drawMap(world, data) {
         drawMap(world, data);
     })
 }
-//line chart of Happiness Score vs Crime Data for selected Country
+//Charts for Happiness Score vs Crime Data for selected Country
+//constants for margins and widths for different charts
 const margin = {top: 20, left: 30, right: 30, bottom: 30}
 const dualAxisWidth = (width / 2) - (margin.left + margin.right) ;
 const dualAxisHeight = (height / 2) - (margin.top + margin.bottom);
-
+const chartWidth = (width / 2) - (margin.left + margin.right) ;
+const chartHeight = (height / 2) - (margin.top + margin.bottom);
+//SVG for dualAxis graph
 const dualAxis = d3.select('body')
     .append('svg')
     .attr('width', dualAxisWidth + margin.left + margin.right)
     .attr('height', dualAxisHeight + margin.top + margin.bottom)
-    //.attr("viewBox", (width/2) + " 10 " + dualAxisWidth + " " + dualAxisHeight)
     .append('g')
         .attr('transform', 'translate(' + margin.left + "," + margin.top + ")");
-//file paths for data
-const Happiness = "../data/ConsolidatedHappiness.csv"
-const Crime = "../data/IntentionalHomicidesFormatted.csv"
+//SVG for bar chart
+const barChart = d3.select('body')
+    .append('svg')
+    .attr('width', chartWidth + margin.left + margin.right)
+    .attr('height', chartHeight + margin.top + margin.bottom)
+    .append('g')
+        .attr('transform', 'translate(' + margin.left + "," + margin.top + ")");
+
+//file path for data
+const Happiness = "./data/ConsolidatedHappinessWithHomicideData.csv" 
 //open files
 Promise.all([
-    d3.csv(Happiness),
-    d3.csv(Crime)
+    d3.csv(Happiness)
 ]).then(data => {
     const happiness_data = data[0];
-    const crime_data = data[1];
-    //convert column formats for crime data
-    crime_data.forEach( d => {
-        d.country = d.country;
-        d.Year = new Date(d.Year, 0);
-        d.NumberOfHomicides = +d.NumberOfHomicides
-        d.RateOfHomicides = +d.RateOfHomicides
-    })
-    //convert column formats for happiness data, plus change name of Country or region column to country to match crime data
+    //convert column formats for happiness data
     happiness_data.forEach( d => {
-        d.Year = new Date(d.Year, 0);
-        d['Happiness rank'] = +d['Happiness rank']
-        d['country'] = d['Country or region']
+        d['Country'] = d['country']
+        d.Year = +d.Year;
+        d['HappinessRank'] = +d['Happiness rank']
         d.Score = +d.Score
         d['GDP per capita'] = +d['GDP per capita']
         d['Social support Index'] = +d['Social support Index']
@@ -184,25 +186,34 @@ Promise.all([
         d['Freedom Index'] = +d['Freedom Index']
         d['Generosity Index'] = +d['Generosity Index']
         d['Corruption Index'] = +d['Corruption Index']
+        d['NumberOfHomicides'] = +d['NumberOfHomicides']
+        d['RateOfHomicides'] = +d['RateOfHomicides']
     })
+
 
     //update this when you add ability to click on a country
     let selectedCountry = 'Colombia'
-
-    let selectedCrimeData = crime_data.filter(function (a) {return a.country === selectedCountry;});
-    let selectedHappinessData = happiness_data.filter(function (a) {return a.country === selectedCountry;});
-
-    console.log(selectedHappinessData)
-    console.log(selectedCrimeData)
+    let selectedHappinessData = happiness_data.filter(function (a) {return a['Country'] === selectedCountry;});
     
+    let selectedHappinessDataYear = happiness_data.filter(function (a) {return (a['Year'] === +selectedYear && a['Country'] === selectedCountry)});
+
+    const barColumns = ['GDP per Capita', 'Social Suport Index', 'Healthy Life Expectancy', 'Freedom Index', 'Generosity Index', 'Corruption Index', 'Crime Rate per 100K']
+    barData = {}
+    for (var i = 0; i < selectedHappinessDataYear.length; i++) {
+
+    }
+    
+    console.log(selectedHappinessDataYear)
+    console.log(barColumns)
     
     //find min and max for use with scales
-    const year_min_max = d3.extent(crime_data, d => new Date(d.Year))
-    const crime_max = d3.max(crime_data, d => d.RateOfHomicides)
+    const year_min_max = d3.extent(happiness_data, d => +d.Year)
+    const crime_max = d3.max(happiness_data, d => +d.RateOfHomicides)
     const crime_min_max = [0, +crime_max]
-    const happiness_min_max = d3.extent(happiness_data, d => +d['Happiness rank'])
+    const happiness_min_max = d3.extent(happiness_data, d => +d.HappinessRank)
+
     //add scales
-    const xScale = d3.scaleTime()
+    const xScale = d3.scaleLinear()
         .domain(year_min_max)
         .range([margin.left, `${dualAxisWidth}` - margin.right]);
     const y1Scale = d3.scaleLinear()
@@ -212,13 +223,13 @@ Promise.all([
         .domain(happiness_min_max)
         .range([`${dualAxisHeight}` - margin.bottom, margin.top]);
     //add axis generators
-    const xAxis = d3.axisBottom().scale(xScale);
+    const xAxis = d3.axisBottom().scale(xScale).ticks(5).tickFormat(d3.format("d"));
     const y1Axis = d3.axisLeft().scale(y1Scale);
     const y2Axis = d3.axisRight().scale(y2Scale);
     //append x axis
     dualAxis.append('g')
         .attr('class', 'axis')
-        .attr('transform', "translate(0," + y1Scale(0) + ")")
+        .attr('transform', "translate(0," + y2Scale(0) + ")")
         .call(xAxis)
         .selectAll('text')
         .style('text-anchor', 'end')
@@ -263,19 +274,18 @@ Promise.all([
         .y(d => y1Scale(d.RateOfHomicides))
     //line generator for happiness
     const lineGenHappy = d3.line()
-        .x(d => xScale(d.x))
-        .y(d => y2Scale(d.y))
-        .defined(function (d) {return d.value;})
+        .x(d => xScale(d.Year))
+        .y(d => y2Scale(d.HappinessRank))
     //Bind data and add path elements for crime data
     const lineChartCrime = dualAxis.append('g')
         .attr('class', 'CrimeLine')
         .selectAll('lines')
-        .data([selectedCrimeData])
+        .data([selectedHappinessData])
         .enter()
         .append('path')
         .attr('d', d => lineGenCrime(d))
         .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
+        .attr('stroke', 'red')
         .attr('stroke-width', 2)
     //Bind data and add path elements for happiness data
     const lineChartHappy = dualAxis.append('g')
@@ -286,8 +296,80 @@ Promise.all([
         .append('path')
         .attr('d', d => lineGenHappy(d))
         .attr('fill', 'none')
-        .attr('stroke', 'red')
+        .attr('stroke', 'steelblue')
         .attr('stroke-width', 2)
+
+    //bar chart
+    //find min and max vals for use in scales
+    const xMax = d3.max(happiness_data, function (d) {
+        return (Math.max(
+            d['GDP per capita'],
+            d['Social support Index'],
+            d['Healthy life expectancy'],
+            d['Freedom Index'],
+            d['Generosity Index'],
+            d['Corruption Index'],
+            d['RateOfHomicides']
+        ))
+    })
+    const xMin = d3.min(happiness_data, function (d) {
+        return (Math.min(
+            d['GDP per capita'],
+            d['Social support Index'],
+            d['Healthy life expectancy'],
+            d['Freedom Index'],
+            d['Generosity Index'],
+            d['Corruption Index'],
+            d['RateOfHomicides']
+        ))
+    })
+    const xMinMax = [xMin, xMax]
+
+    //create scales
+    const xBarScale = d3.scaleLinear()
+        .domain(xMinMax)
+        .range([margin.left, `${dualAxisWidth}` - margin.right]);
+    const yBarScale = d3.scaleLinear()
+        .range([`${dualAxisHeight}` - margin.bottom, margin.top]);
+    //add axis generators
+    const xBarAxis = d3.axisBottom().scale(xBarScale);
+    const yBarAxis = d3.axisLeft().scale(yBarScale).tickFormat('').tickSize(0);
+    //add x axis
+    barChart.append('g')
+        .attr('class', 'axis')
+        .attr('transform', "translate(0," + yBarScale(0) + ")")
+        .call(xBarAxis)
+        .selectAll('text')
+        .style('text-anchor', 'end')
+        .attr('dx', '-0.8em')
+        .attr('dy', '0.15em')
+        .attr('transform', 'rotate(-65)')
+    //append y axis
+    barChart.append('g')
+        .attr('class', 'axis')
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(yBarAxis)
+    //add x axis label
+    barChart.append('text')
+        .attr('class', 'axisLabel')
+        .attr('text-anchor', 'end')
+        .attr('x', dualAxisWidth/2)
+        .attr('y', dualAxisHeight + margin.top + 10)
+        .text('Value')
+
+    //bars
+    barChart.selectAll('myRect')
+        .data([barData])
+        .enter()
+        .append('rect')
+        .attr('x', xBarScale(0))
+        .attr('y', function (d) {return selectedHappinessDataYear.keys.length})
+        .attr('width')
+        
+    
+    
+    
+    
         
 
 
